780dbb77ebe4c499f8b62d27a495ba07a0fdae64
qFioofa <penavana1@gmail.com>
2026-06-26 18:45:35 +0300

    Deleted unused routs

--- frontend/src/routes/players/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	if (import.meta.env.MODE === "production") {
-		throw redirect(404, "/");
-	}
-	import PlayerComp from "$lib/elements/PlayerComp.svelte";
-</script>
-
-<PlayerComp />
--- frontend/src/routes/timer/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	if (import.meta.env.MODE === "production") {
-		throw redirect(404, "/");
-	}
-	import Timer from "$lib/elements/Timer.svelte";
-</script>
-
-<Timer />
